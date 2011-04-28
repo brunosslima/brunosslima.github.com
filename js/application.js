b8e876642a1b06978d009018250b239acb81ccf4
Rafael Santos <rafaelsantos@xlii.com.br>
2011-04-27 22:45:49 -0300

    atualização do cross browsing usando jsonp

--- js/application.js
+++ js/application.js
@@ -1,6 +1,22 @@
-$(function(){
-  $.getJSON('http://gist.github.com/api/v1/json/gists/formigarafa', function(data){
-    alert(data.gists[0].repo);
-  })
+gist_list = []
+register_gists = function (list) {
+  gist_list = list.gists;
 }
-)
+
+$(function() {
+  $.each(gist_list, function(index, gist) {
+    var element = $('<li/>');
+    element.append($('<h2/>').html(gist.description));
+    element.append($('<span/>').attr('style', 'display: block').html(gist.created_at));
+    element.append($('<span/>').html('Comentários: '+gist.comments.length));
+    
+    var files = $('<div/>').attr('class', 'files');
+
+    $.each(gist.files, function(file_index, file) {
+      files.append($('<a/>').attr('href', 'https://gist.github.com/' + gist.repo + '#' + file).html(file))
+    });
+    
+    element.append(files);
+    $('.gists').append(element);
+  });
+})
