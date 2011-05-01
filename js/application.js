2ed5270ad4dd9951a940fec5a9582aa41084611f
Rafael Santos <rafaelsantos@xlii.com.br>
2011-05-01 03:30:04 -0300

    exibição dos gists no corpo do blog

--- js/application.js
+++ js/application.js
@@ -6,17 +6,49 @@ register_gists = function (list) {
 $(function() {
   $.each(gist_list, function(index, gist) {
     var element = $('<li/>');
+    element.attr('data-repo', gist.repo);
     element.append($('<h2/>').html(gist.description));
     element.append($('<span/>').attr('style', 'display: block').html(gist.created_at));
     element.append($('<span/>').html('Comentários: '+gist.comments.length));
     
     var files = $('<div/>').attr('class', 'files');
 
-    $.each(gist.files, function(file_index, file) {
-      files.append($('<a/>').attr('href', 'https://gist.github.com/' + gist.repo + '#file_' + file).html(file))
+    $.each(gist.files, function(file_index, filename) {
+      var file = $('<div/>');
+      file.attr('data-filename', filename);
+      var file_link = $('<a/>').attr('href', 'https://gist.github.com/' + gist.repo + '#file_' + filename).html(filename);
+      file.append(file_link);
+      schedule_gist_script(gist.repo, filename);
+      files.append(file);
     });
     
     element.append(files);
     $('.gists').append(element);
   });
+  activate_write_grabber();
 })
+
+schedule_gist_script = function (repo, file) {
+  var script   = document.createElement("script");
+  script.type  = "text/javascript";
+  script.src   = "https://gist.github.com/"+repo+".js?file="+encodeURIComponent(file);
+  document.body.appendChild(script);
+}
+
+activate_write_grabber = function() {
+  document.write = function(script_text) {
+    var content = $(script_text);
+
+    if(content.find('div').length > 0) {
+      insert_gist(content);
+    }
+  } 
+}
+
+insert_gist = function(gist) {
+  var repo_link = gist.find('a:contains(This Gist)');
+  var repo = repo_link.attr('href').replace("https://gist.github.com/", "");
+  var file_link = gist.find('a[href^="https://gist.github.com/'+repo+'#"]');
+  var file_name = file_link.attr('href').replace('https://gist.github.com/'+repo+'#file_', "");  
+  $("li[data-repo="+repo+"] .files div[data-filename='"+file_name+"']").append(gist);
+}
